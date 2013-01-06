70327aeea506957dc4bb4423ebf3493441953336
tinaxu <949330215@qq.com>
2013-01-06 18:20:56 +0800

    commit by 1/6

--- Prototype/js/all.js
+++ Prototype/js/all.js
@@ -1,24 +1,79 @@
 $(function() {
-	$("#loginButton").click(function(event) {
-		$("#authorcontent").css("display", "none");
-		$("#homecontent").show();
-		$("#nav > .navItem").removeClass("navActive");
-		$("#home").addClass("navActive")
-	})
+	//page refesh , remember this page
+	window.onunload=function(){
+		
+	}	   
+	window.onload=function(){
+		//$("#nav #dash").click();
+		}		   
+    // show or hide user infor (img setting)
+	showHideUserInfor();
+	function showHideUserInfor(){
+		  if($("#authorcontent").css("display")=="block"){
+		   $("#userInfor").hide();
+		   }else{
+			    $("#userInfor").show();
+			   }
+			   }
+			   
+	//click "enter" key		   
+	$("input[name='password']").keydown(function(e) {
+		if (e.keyCode == 13) {
+			$("#loginButton").click();
+		}
+	});
+	// login event 
+	$("#loginButton")
+			.click(
+					function(event) {
+						var userAccout = $("input[name='userAccout']").val();
+						var password = $("input[name='password']").val();
+						if (userAccout == "") {
+							$("input[name='userAccout']").focus();
+							$("#userLogin span").first().show();
+						} else {
+							$("#userLogin span").first().hide();
+							if (password == "") {
+								$("input[name='password']").focus();
+								$("#userLogin span").last().show();
+							} else if ((password != "" && password !== "admin")
+									|| (userAccout != "" && userAccout !== "admin")) {
+								$("#userLogin span")
+										.html(
+												"The  User Account or password you entered is incorrect.")
+										.last().show();
+							}
 
-	//change nav
+							if (userAccout == "admin" && password == "admin") {
+								$("#authorcontent").css("display", "none");
+								showHideUserInfor();
+								$("#fristName").html(userAccout)
+								$("#lastName").html(userAccout)
+								//$("#userImg img").first().attr("src",
+								//		"image/photo.png");
+								$("#homecontent").show();
+								$("#nav > .navItem").removeClass("navActive");
+								$("#home").addClass("navActive")
+							}
+						}
+					})
+
+
+
+	// change nav
 	$("#nav > .navItem").click(function(event) {
 		event.stopPropagation();
 		$("#nav > .navItem").each(function(i) {
 			if ($(this).hasClass("navActive")) {
-				$("#" + $(this).attr("id") + ("content")).hide()
+				$("#" + $(this).attr("id") + ("content")).hide();
 			}
 		});
 		$("#nav > .navItem").removeClass("navActive");
 
 		$(this).addClass("navActive");
 
-		$("#" + $(this).attr("id") + ("content")).show()
+		$("#" + $(this).attr("id") + ("content")).show();
+		showHideUserInfor();
 	});
 
 	// start click
@@ -35,50 +90,60 @@ $(function() {
 		$(".dropdown-menu").hide();
 	})
 
-	//show tips 
+	// show tips
 	$("ul li").mouseenter(
 			function() {
-				$(this).find(".tips").find(".value").html(
-						$(this).find(".earlierdata").text());
+			var div=$(".tips")
+			var self = $(this); 
+				$(this).find(".tips").html(
+						$(this).find(".earlierdata").text()+" updated items");
+				var p = self.position()  
+        var x = p.left + self.width()/2; 
+        var docWidth = $(document).width();
+		var y=p.top-self.height();
+        div.css("left", x);  
+        div.css("top", y);  
 				$(this).find(".tips").show();
 			}).mouseleave(function() {
 		$(this).find(".tips").hide();
 	});
 
+
 	// home page nav
-	//social dash board 
+	// social dash board
 	$("#socialDash").click(function() {
 		$("#nav #dash").click();
 	})
 
-	//social network insight 
+	// social network insight
 	$("#socialNetwork").click(function() {
 		$("#nav #network").click();
 	})
 
-	//social campaingn plan 
+	// social campaingn plan
 	$("#socialCampaign").click(function() {
 		$("#nav #plan").click();
 	})
 
-	//activity automation 
+	// activity automation
 	$("#activityAutomation").click(function() {
 		$("#nav #activity").click();
 	})
 
-	//media mediation
+	// media mediation
 	$("#mediaMediation").click(function() {
 		$("#nav #media").click();
 	})
 
-	//channel maintenace
+	// channel maintenace
 	$("#channelMaintenace").click(function() {
 		$("#nav #channel").click();
 	})
-	//even table rows style
-	$("#socialTable tr:odd").css("background-color", "#FFFFFF");
+	// even table rows style
+	$(".socialTable tr:odd").css("background-color", "#FFFFFF");
+	$(".sentimentTable tr:odd").css("background-color", "#FFFFFF");
 
-	// socail dash board -anlaysis 
+	// socail dash board -anlaysis
 	// area click
 	$("#area").click(function() {
 		$("#overview").addClass("overview1");
