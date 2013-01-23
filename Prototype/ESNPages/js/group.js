620f3ceec6b34b4b5720c6b2d523e6f4b90da58d
tinaxu <949330215@qq.com>
2013-01-23 12:19:40 +0800

    6-10 nav

--- Prototype/ESNPages/js/group.js
+++ Prototype/ESNPages/js/group.js
@@ -1,8 +1,15 @@
 $(function() {
 //scroll to my content
-$('.navList li').click(function(){
- window.scrollTo($('#experts').position().left,$('#experts').position().top);
+$('.navList li').click(function(){	
+	$('.navList li').each(function(){
+	$(this).find("a").find("img").attr("src","image/nav.png")
+	$(this).find("a").css("margin-right","-190px")
+								   })	
+$(this).find("a").find("img").attr("src","image/nav3.png")
+$(this).find("a").css("margin-right","-165px")
+var myclass=$(this).find("a").attr('class')
+var content =myclass+("Content")
+ window.scrollTo($('.'+content).position().left,$('.'+content).position().top-88);
 return false;
 });
-	
 		   });
