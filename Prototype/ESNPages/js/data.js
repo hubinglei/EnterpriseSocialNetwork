144addf036deb7d3b3a1337dae266041d1673e39
tinaxu <949330215@qq.com>
2013-01-24 14:26:21 +0800

    updated

--- Prototype/ESNPages/js/data.js
+++ Prototype/ESNPages/js/data.js
@@ -36,6 +36,26 @@
 						  ],xlabel:['Month1','Month2','Month3','Month4','Month5','Month6','Month7','Month8','Month9','Month10']}
   }
   
+  
+  function loadDay2(target,options){
+	  return {data:[
+						  [51, 90, 42, 89, 45, 87, 50,83, 43, 82],
+						  [50, 82, 40, 83, 43, 82, 45, 30, 74, 32]
+						  ],xlabel:['Day1','Day2','Day3','Day4','Day5','Day6','Day7','Day8','Day9','Day10']}
+  }
+  function loadWeek2(target,options){
+	  return {data:[
+	                	  [50, 90, 42, 89, 45, 87, 50,50, 90, 42],
+	                      [49, 82, 38, 80, 25, 80, 40, 80, 25, 80]
+						  ],xlabel:['Week1','Week2','Week3','Week4','Week5','Week6','Week7','Week8','Week9','Week10']}
+  }
+  function loadMonth2(target,options){
+	  return {data:[  	  
+	                  	  [50, 90, 42, 89, 45, 87, 50,42, 89, 45],   
+						  [49, 82, 35, 87, 41, 80, 42,82, 35, 87]
+						  ],xlabel:['Month1','Month2','Month3','Month4','Month5','Month6','Month7','Month8','Month9','Month10']}
+  }
+  
   function loadDayMap(target,options){
 	  return {data:[{name:'XJ',value:10},
 	                {name:'XZ',value:20},
--- Prototype/ESNPages/js/group.js
+++ Prototype/ESNPages/js/group.js
@@ -9,6 +9,7 @@ $(this).find("a").find("img").attr("src","image/nav3.png")
 $(this).find("a").css("margin-right","-165px")
 var myclass=$(this).find("a").attr('class')
 var content =myclass+("Content")
+alert(myclass)
  window.scrollTo($('.'+content).position().left,$('.'+content).position().top-88);
 return false;
 });
