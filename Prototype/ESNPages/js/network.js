792afd0f44538d01ffaecf566f96a90aff89a7d9
tinaxu <949330215@qq.com>
2013-01-25 16:56:34 +0800

    campaign and group updated

--- Prototype/ESNPages/js/network.js
+++ Prototype/ESNPages/js/network.js
@@ -1,94 +1,92 @@
 $(function() {
-page="networkNav"	
-$("#influenceAnalysis").click(function() {
-		pos="dash"
+	page = "networkNav"
+	$("#influenceAnalysis").click(function() {
+		pos = "dash"
 		networkNav2("networkInluence");
 	})
 
 	// social network insight
 	$("#sentimentAnalysis").click(function() {
-		pos="network"
+		pos = "network"
 		networkNav2("networkSentiment");
 	})
 	// media mediation
 	$("#costAnalysis").click(function() {
-	 pos="media"
-	 networkNav2("networkCost");
+		pos = "media"
+		networkNav2("networkCost");
 	})
-	
+
 	$(".arrowRight1").click(function() {
 		moveRight();
 		$(".arrowleft1 ").show();
 		$(".networkArrowRight ").hide();
-								});
-	
-		$(".arrowLeft1").click(function() {
+	});
+
+	$(".arrowLeft1").click(function() {
 		moveLeft();
 		$(".arrowleft1 ").hide();
-			$(".networkArrowRight ").show();
-								});
-	
-	
-	function moveRight(){
+		$(".networkArrowRight ").show();
+	});
+
+	function moveRight() {
 		$(".networkRightNav").css("margin-left", "-10px");
 		$(".networkRightNav").css("opacity", "1");
 		$(".rightNav").css("margin-right", "1290px");
 		$("#inluenceCompanyGroup").click(function() {
-								 $("#wrapper").load("companyGroup.html");	
-											  });
-	$("#inluenceCompany").click(function() {
-								 $("#wrapper").load("company.html");	
-											  });
-	
-	$("#inluenceBrandGroup").click(function() {
-								 $("#wrapper").load("brandGroup.html");	
-											  });
-	$("#inluenceBrand").click(function() {
-								 $("#wrapper").load("brand.html");	
-											  });
-	$("#inluenceProductGroup").click(function() {
-								 $("#wrapper").load("productGroup.html");	
-											  });
-	$("#inluenceProduct").click(function() {
-								 $("#wrapper").load("product.html");	
-											  });
-		}
-		
-		
+			$("#wrapper").load("companyGroup.html");
+		});
+		$("#inluenceCompany").click(function() {
+			$("#wrapper").load("company.html");
+		});
+
+		$("#inluenceBrandGroup").click(function() {
+			$("#wrapper").load("brandGroup.html");
+		});
+		$("#inluenceBrand").click(function() {
+			$("#wrapper").load("brand.html");
+		});
+		$("#inluenceProductGroup").click(function() {
+			$("#wrapper").load("productGroup.html");
+		});
+		$("#inluenceProduct").click(function() {
+			$("#wrapper").load("product.html");
+		});
+
 		$("#sentimentCompany").click(function() {
-											   $("#wrapper").load("companySentiment.html");
-											  });
-			$("#sentimentBrand").click(function() {
-												$("#wrapper").load("brandSentiment.html");
-											  });
-				$("#sentimentProduct").click(function() {
-											 $("#wrapper").load("productSentiment.html");		 
-											  });
-		
-			$("#costCompany").click(function() {
-											   $("#wrapper").load("companyCost.html");
-											  });
-			$("#costBrand").click(function() {
-												$("#wrapper").load("brandCost.html");
-											  });
-				$("#costProduct").click(function() {
-											 $("#wrapper").load("productCost.html");		 
-											  });
-				
-				$("#inluenceCampaign").click(function() {
-													  $("#wrapper").load("campaign.html");
-													   });
-				$("#inluenceCampaignGroup").click(function() {
-														     $("#wrapper").load("campaignGroup.html");
-													   });
-				
-				$("#costCampaign").click(function() {
-													$("#wrapper").load("campaignCost.html");
-													   });
-		
-	function moveLeft(){
+			$("#wrapper").load("companySentiment.html");
+		});
+		$("#sentimentBrand").click(function() {
+			$("#wrapper").load("brandSentiment.html");
+		});
+		$("#sentimentProduct").click(function() {
+			$("#wrapper").load("productSentiment.html");
+		});
+
+		$("#costCompany").click(function() {
+			$("#wrapper").load("companyCost.html");
+		});
+		$("#costBrand").click(function() {
+			$("#wrapper").load("brandCost.html");
+		});
+		$("#costProduct").click(function() {
+			$("#wrapper").load("productCost.html");
+		});
+
+		$("#inluenceCampaign").click(function() {
+			$("#wrapper").load("campaign.html");
+		});
+		$("#inluenceCampaignGroup").click(function() {
+			$("#wrapper").load("campaignGroup.html");
+		});
+
+		$("#costCampaign").click(function() {
+			$("#wrapper").load("campaignCost.html");
+		});
+	}
+
+	function moveLeft() {
 		$(".networkRightNav").css("margin-left", "850px");
 		$(".networkRightNav").css("opacity", "0.5");
 		$(".rightNav").css("margin-right", "890px");
-		}
-		   });
+	}
+});
