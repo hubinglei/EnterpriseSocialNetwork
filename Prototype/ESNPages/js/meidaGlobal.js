7df0c4837b477085c05c2e7ac80b1703a6f69156
tinaxu <949330215@qq.com>
2013-03-04 18:05:36 +0800

    fixed bugs

--- Prototype/ESNPages/js/meidaGlobal.js
+++ Prototype/ESNPages/js/meidaGlobal.js
@@ -54,7 +54,7 @@ $(function() {
 	$('#addMedia')
 			.click(
 					function() {
-						var mediaHtml = '<div class="mediaGroup newGroup"><div class="addMediatitle">Media Name</div><div class="click_down"></div><hr /><div class="descript"><div class="interface"></div><div class="filter"></div><div class="right"></div><div class="open"></div><p><img src="image/expand.png" />Media Name</p><input type="text" value="Add Media Name"></p><p><img src="image/expand.png" />Comment</p><input type="text" value="Add Comment"></p><p><img src="image/expand.png" />Related Entity</p><input type="text" value="Add Related Entity"></p><p><img src="image/expand.png" />Short Message</p><input type="text" value="Add Short Message"></p><p><img src="image/expand.png" />Long Message</p><input type="text" style="height:100px;" value="Add Long Message"></p><p><img src="image/expand.png" />Picture</p><p><img src="image/addpicture.png" id="addpicture"/><input type="file" id="addpictureInput" style="display:none"></p><img src="image/expand.png" />Video</p><img src="image/addvideo.png" id="addvideo" /><input type="file" id="addvideoInput" style="display:none"><p><img src="image/expand.png" />URL</p><input type="text" value="Add URL"><img src="image/addbutton1.png" class="addbutton" /></p><p><img src="image/expand.png" />Document</p><div class="addDocument">Add Document</div><p><img src="image/expand.png" />Vote</p><input type="text" value="Add Vote"></p><p><img src="image/expand.png" />Media Release</p><div class="imgList"><img src="image/twitter.png" /><img src="image/facebook.png" /><img src="image/onehp.png" /><img src="image/onehp.png" /><img src="image/hpconnect.png" /><img src="image/linkedin.png" /><img src="image/sharepoint.png" /></div><div class="cancleButton">Cancle</div><div class="saveButton">OK</div>';
+						var mediaHtml = '<div class="mediaGroup newGroup"><div class="addMediatitle">Media Name</div><div class="click_down"></div><hr /><div class="descript"><div class="interface"></div><div class="filter"></div><div class="right"></div><div class="open"></div><p><img src="image/expand.png" />Media Name</p><input type="text" value="Add Media Name"></p><p><img src="image/expand.png" />Comment</p><input type="text" value="Add Comment"></p><p><img src="image/expand.png" />Related Entity</p><input type="text" value="Add Related Entity"></p><p><img src="image/expand.png" />Short Message</p><input type="text" value="Add Short Message"></p><p><img src="image/expand.png" />Long Message</p><input type="text" style="height:100px;" value="Add Long Message"></p><p><img src="image/expand.png" />Picture</p><p><img src="image/addpicture.png" id="addpicture"/><input type="file" id="addpictureInput" style="display:none"></p><img src="image/expand.png" />Video</p><img src="image/addvideo.png" id="addvideo" /><input type="file" id="addvideoInput" style="display:none"><p><img src="image/expand.png" />URL</p><input type="text" value="Add URL"><img src="image/addbutton1.png" class="addbutton" /></p><p><img src="image/expand.png" />Document</p><div class="addDocument">Add Document</div><p><img src="image/expand.png" />Vote</p><input type="text" value="Add Vote"></p><p><img src="image/expand.png" />Media Release</p><div class="imgList"><img src="image/twitter1.png" /><img src="image/facebook1.png" /><img src="image/onehp1.png" /><img src="image/hpconnect1.png" /><img src="image/linkedin1.png" /><img src="image/sharepoint1.png" /></div><div class="cancleButton">Cancle</div><div class="saveButton">OK</div>';
 						if (!$('.mediaGroup').last().hasClass("newGroup")) {
 							$(".navigate").before(mediaHtml);
 						}
@@ -79,7 +79,17 @@ $(function() {
 						$(".saveButton").click(function(){
 								$(".newGroup").remove();
 														})
-
+						
+						
+								$(".imgList img").click(function(){
+											var imgSrc =$(this).attr('src');
+											if(imgSrc.indexOf('_selected')>-1){
+												$(this).attr('src',imgSrc.replace('_selected',''))
+												}else{
+													$(this).attr('src',imgSrc.replace('.png','_selected.png'))
+													}
+											})
+								
 					})
 			
 			
